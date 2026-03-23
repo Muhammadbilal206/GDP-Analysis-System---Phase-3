346dbead3a0875b9bfb93298ea2ef7ad24d1bf5e
Muhammadbilal206 <63650308+Muhammadbilal206@users.noreply.github.com>
2026-03-24 00:23:02 +0500

    Refactor InputModule to DataIngestor with enhancements

--- plugins/input_module.py
+++ plugins/input_module.py
@@ -1,21 +1,34 @@
 import csv
 import time
 
-class InputModule:
-    def __init__(self, config):
-        self.config = config
+TYPE_MAP = {
+    "string": str,
+    "integer": int,
+    "float": float
+}
 
-    def run(self, raw_queue):
-        with open(self.config['dataset_path'], 'r') as f:
-            reader = csv.DictReader(f)
-            cols = self.config['schema_mapping']['columns']
-            for row in reader:
-                packet = {}
-                for c in cols:
-                    val = row[c['source_name']]
-                    if c['data_type'] == 'float': val = float(val)
-                    elif c['data_type'] == 'integer': val = int(val)
-                    packet[c['internal_mapping']] = val
-                raw_queue.put(packet)
-                time.sleep(self.config['pipeline_dynamics']['input_delay_seconds'])
-        raw_queue.put(None)
+class DataIngestor:
+    def __init__(self, config_data):
+        self.filepath = config_data["dataset_path"]
+        self.schema = config_data["schema_mapping"]["columns"]
+        self.sleep_time = config_data["pipeline_dynamics"]["input_delay_seconds"]
+
+    def execute(self, q_out, worker_count):
+        with open(self.filepath, mode="r", encoding="utf-8") as file_obj:
+            csv_reader = csv.DictReader(file_obj)
+            
+            for index, row_data in enumerate(csv_reader):
+                parsed_record = self._transform(row_data, index)
+                q_out.put(parsed_record)
+                time.sleep(self.sleep_time)
+
+        for _ in range(worker_count):
+            q_out.put(None)
+
+    def _transform(self, row_data, index):
+        record = {"_seq": index}
+        for field in self.schema:
+            raw_val = row_data[field["source_name"]]
+            cast_func = TYPE_MAP[field["data_type"]]
+            record[field["internal_mapping"]] = cast_func(raw_val)
+        return record
