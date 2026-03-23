778935a3159dad4fa40ecbdf632483a3589067d0
Muhammadbilal206 <63650308+Muhammadbilal206@users.noreply.github.com>
2026-03-24 00:23:54 +0500

    Refactor Aggregator to StreamReassembler with new logic

--- core/aggregator.py
+++ core/aggregator.py
@@ -1,12 +1,41 @@
-from core.functional_core import FunctionalCore
+from .functional_core import calc_moving_mean
 
-class Aggregator:
-    def __init__(self, window_size):
-        self.window_size = window_size
+class StreamReassembler:
+    def __init__(self, config_data, worker_count):
+        self.limit = config_data["processing"]["stateful_tasks"]["running_average_window_size"]
+        self.worker_count = worker_count
         self.history = []
+        self.stash = {}
+        self.expected_seq = 0
 
-    def process(self, value):
-        self.history.append(value)
-        if len(self.history) > self.window_size:
-            self.history.pop(0)
-        return FunctionalCore.calculate_average(self.history)
+    def execute(self, q_in, q_out):
+        end_signals = 0
+        
+        while True:
+            item = q_in.get()
+            
+            if item is None:
+                end_signals += 1
+                if end_signals >= self.worker_count:
+                    self._purge_stash(q_out)
+                    q_out.put(None)
+                    break
+                continue
+
+            self.stash[item["_seq"]] = item
+            self._purge_stash(q_out)
+
+    def _purge_stash(self, q_out):
+        while self.expected_seq in self.stash:
+            current_item = self.stash.pop(self.expected_seq)
+            self.expected_seq += 1
+
+            if current_item.get("_dropped"):
+                continue
+
+            self.history.append(current_item["metric_value"])
+            if len(self.history) > self.limit:
+                self.history.pop(0)
+
+            current_item["computed_metric"] = calc_moving_mean(self.history)
+            q_out.put(current_item)
